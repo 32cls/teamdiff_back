9fa12231febccc8500452ae27b511c91b0b810d3
mvvtf <code@marius.wtf>
2025-05-01 13:14:13 +0200

    clean factories and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            AccountSeeder::class,
-            SummonerSeeder::class,
-            ReviewSeeder::class,
-        ]);
+        $this->call([]);
 
     }
 }
