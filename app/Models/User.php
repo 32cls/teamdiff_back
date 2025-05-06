2c263543bfe2796100a6d9b0048680926e08d70e
mvvtf <code@marius.wtf>
2025-05-07 01:40:33 +0200

    make user authenticable cauz i forgor

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
-class User extends Model
+class User extends Model implements AuthenticatableContract
 {
+    use Authenticatable;
     use HasFactory;
     use HasTimestamps;
     use HasUlids;
